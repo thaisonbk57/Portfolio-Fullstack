69d9e8b1f6c94f2c51cc5af7fd749dff786dee79
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-11-29 22:11:00 +0100

    minor changes

--- src/components/UI/SocialIcon/SocialIcon.js
+++ src/components/UI/SocialIcon/SocialIcon.js
@@ -6,7 +6,12 @@ export default function SocialIcon(props) {
   const { name, color, link } = props;
 
   return (
-    <a className="SocialIcon" href={link} target="_blank">
+    <a
+      className="SocialIcon"
+      href={link}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
       <i style={{ color }} className={name} />
     </a>
   );
