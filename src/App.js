5e8f796616b71f1062ea3c4b8520c41009b0b98e
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-12-01 00:18:18 +0100

    created 404 Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import MyWorks from "./pages/MyWorks/MyWorks";
 import Contact from "./pages/Contact/Contact";
+import Page404 from "./pages/404/404";
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
             <Route path="/about" exact component={About} />
             <Route path="/projects" exact component={MyWorks} />
             <Route path="/contact" exact component={Contact} />
+            <Route component={Page404} />
           </Switch>
         </Layout>
       </div>
---
+++ src/pages/404/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./404.scss";
+
+export default function Page404() {
+  return (
+    <div className="Page404">
+      <h1>404 not found!</h1>
+    </div>
+  );
+}
