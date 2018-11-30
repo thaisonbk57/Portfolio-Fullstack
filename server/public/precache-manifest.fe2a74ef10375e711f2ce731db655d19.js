3793926d094fe926044d510385cb727ce06f8614
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-12-01 00:24:37 +0100

    build project

---
+++ server/public/precache-manifest.fe2a74ef10375e711f2ce731db655d19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfcba4788df7b019014eb4628eaa464b",
+    "url": "/static/media/p1.cfcba478.jpeg"
+  },
+  {
+    "revision": "2c58897a74b7316c9443acc494376ff5",
+    "url": "/static/media/bg-home.2c58897a.jpeg"
+  },
+  {
+    "revision": "41af6a24fb5902299ca3138831e1303d",
+    "url": "/static/media/avatar.41af6a24.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a223e8d723f15f83f7a",
+    "url": "/static/js/main.0a223e8d.chunk.js"
+  },
+  {
+    "revision": "abe8e7fe50c58729887d",
+    "url": "/static/js/1.abe8e7fe.chunk.js"
+  },
+  {
+    "revision": "0a223e8d723f15f83f7a",
+    "url": "/static/css/main.0842576a.chunk.css"
+  },
+  {
+    "revision": "a2a806ff9ae72c0d7516a57641c19f44",
+    "url": "/index.html"
+  }
+];
