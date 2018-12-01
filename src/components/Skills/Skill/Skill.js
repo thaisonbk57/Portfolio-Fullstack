32c343d70bde209150df2192052ff36486eb4d59
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-12-01 23:27:01 +0100

    animated Skillbar

--- src/components/Skills/Skill/Skill.js
+++ src/components/Skills/Skill/Skill.js
@@ -3,13 +3,16 @@ import React from "react";
 import "./Skill.scss";
 
 export default function Skill(props) {
-  const { skill, level } = props;
+  const { skill, level, id } = props;
 
   return (
     <div className="Skill">
       <p className="Skill-name">{skill}</p>
       <div className="Skill-bar">
-        <div style={{ width: `${level}%` }} className="Skill-level">
+        <div
+          style={{ width: `${level}%` }}
+          className={`Skill-level Skill-level-${id}`}
+        >
           <span>{level}</span>
         </div>
       </div>
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -7,37 +7,49 @@ import Skill from "./Skill/Skill";
 const skills = [
   {
     name: "HTML5",
-    level: "85"
+    level: "85",
+    id: 1
   },
   {
     name: "CSS (SCSS)",
-    level: "80"
+    level: "80",
+    id: 2
   },
   {
     name: "Javascript",
-    level: "80"
+    level: "80",
+    id: 3
   },
   {
     name: "Bootstrap",
-    level: "70"
+    level: "70",
+    id: 4
   },
   {
     name: "ReactJS - Redux",
-    level: "75"
+    level: "75",
+    id: 5
   },
   {
     name: "NodeJS (ExpressJS)",
-    level: "75"
+    level: "75",
+    id: 6
   },
   {
     name: "MongoDB (Mongoose)",
-    level: "60"
+    level: "60",
+    id: 7
   }
 ];
 
 export default function Skills() {
   const skillSet = skills.map(skill => (
-    <Skill key={skill.name} skill={skill.name} level={skill.level} />
+    <Skill
+      key={skill.name}
+      skill={skill.name}
+      level={skill.level}
+      id={skill.id}
+    />
   ));
 
   return <div className="Skill">{skillSet}</div>;
