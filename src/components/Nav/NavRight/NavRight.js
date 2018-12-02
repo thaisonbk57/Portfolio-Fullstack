e83bce7a3a1361f888be6dccd1835e33233f8f1e
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-12-02 10:37:48 +0100

    render NavItem using map function

--- src/components/Nav/NavRight/NavRight.js
+++ src/components/Nav/NavRight/NavRight.js
@@ -4,34 +4,48 @@ import "./NavRight.scss";
 
 import NavItem from "./../NavItem/NavItem";
 
+const navItems = [
+  {
+    label: "HOME",
+    link: "/",
+    exact: true
+  },
+  {
+    label: "ABOUT ME",
+    link: "/about",
+    exact: false
+  },
+  {
+    label: "MY WORKS",
+    link: "/projects",
+    exact: false
+  },
+  {
+    label: "HOW TO REACH ME",
+    link: "/contact",
+    exact: false
+  }
+];
+
 export default function NavRight(props) {
   const { toggleNavHandler } = props;
 
+  const menu = navItems.map((item, index) => {
+    return (
+      <NavItem
+        key={index}
+        toggleNavHandler={toggleNavHandler}
+        link={item.link}
+        label={item.label}
+        exact={item.exact}
+        order={index}
+      />
+    );
+  });
+
   return (
     <div className="NavRight">
-      <ul className="NavBar">
-        <NavItem
-          toggleNavHandler={toggleNavHandler}
-          link={"/"}
-          label={"HOME"}
-          exact
-        />
-        <NavItem
-          toggleNavHandler={toggleNavHandler}
-          link={"/about"}
-          label={"ABOUT ME"}
-        />
-        <NavItem
-          toggleNavHandler={toggleNavHandler}
-          link={"/projects"}
-          label={"MY WORKS"}
-        />
-        <NavItem
-          toggleNavHandler={toggleNavHandler}
-          link={"/contact"}
-          label={"HOW TO REACH ME"}
-        />
-      </ul>
+      <ul className="NavBar">{menu}</ul>
     </div>
   );
 }
