7acbd49c332bcf4ef226ec1a3b3cef821aff90f9
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-12-02 10:44:54 +0100

    animation for NavItem

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 import "./NavItem.scss";
 
 export default function NavItem(props) {
-  const { link, label, exact, toggleNavHandler } = props;
+  const { link, label, exact, toggleNavHandler, order } = props;
   return (
-    <li className="NavItem">
+    <li className={`NavItem NavItem-${order}`}>
       <NavLink onClick={toggleNavHandler} to={link} exact={exact}>
         {label}
       </NavLink>
